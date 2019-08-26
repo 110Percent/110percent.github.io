02fc3fe00e539975a021c7dc29d910fb6b533c60
Davies <curtis.davies@bell.corp.bce.ca>
2019-08-26 16:27:58 -0400

    Initial rework commit

--- include/js/main.js
+++ include/js/main.js
@@ -1,7 +1,11 @@
+const splashes = ['Please do not swear on my profile!!', 'The one and only!!', 'Ladies, please, one at a time.', 'Who needs CSS frameworks?', 'I\'ll write my own library, damn it!', 'Readable code is overrated.', 'Who needs coffee when you\'ve got pure stubbornness?'];
+
+document.querySelector('.splash-text').textContent = splashes[Math.floor(Math.random() * splashes.length)];
+
 tippy('.splash-icon', {
-    placement: 'top',
-    delay: 100,
-    duration: 400,
-    inertia: true,
-    animation: 'shift-away'
-})
+  placement: 'top',
+  delay: 100,
+  duration: 400,
+  inertia: true,
+  animation: 'shift-away'
+});
